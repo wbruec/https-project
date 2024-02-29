480e6ac73b408c6f79f9e39b92502ec6e1ad9aa3
wbruec <wbruec@gmail.com>
2024-02-29 22:11:25 +0100

    add pubkey to pki_helpers

--- pki_helpers.py
+++ pki_helpers.py
@@ -21,3 +21,52 @@ def generate_private_key(filename: str, passphrase: str):
         )
 
     return private_key
+
+# pki_helpers.py
+from datetime import datetime, timedelta
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+
+def generate_public_key(private_key, filename, **kwargs):
+    subject = x509.Name(
+        [
+            x509.NameAttribute(NameOID.COUNTRY_NAME, kwargs["country"]),
+            x509.NameAttribute(
+                NameOID.STATE_OR_PROVINCE_NAME, kwargs["state"]
+            ),
+            x509.NameAttribute(NameOID.LOCALITY_NAME, kwargs["locality"]),
+            x509.NameAttribute(NameOID.ORGANIZATION_NAME, kwargs["org"]),
+            x509.NameAttribute(NameOID.COMMON_NAME, kwargs["hostname"]),
+        ]
+    )
+
+    # Because this is self signed, the issuer is always the subject
+    issuer = subject
+
+    # This certificate is valid from now until 30 days
+    valid_from = datetime.utcnow()
+    valid_to = valid_from + timedelta(days=30)
+
+    # Used to build the certificate
+    builder = (
+        x509.CertificateBuilder()
+        .subject_name(subject)
+        .issuer_name(issuer)
+        .public_key(private_key.public_key())
+        .serial_number(x509.random_serial_number())
+        .not_valid_before(valid_from)
+        .not_valid_after(valid_to)
+        .add_extension(x509.BasicConstraints(ca=True,
+            path_length=None), critical=True)
+    )
+
+    # Sign the certificate with the private key
+    public_key = builder.sign(
+        private_key, hashes.SHA256(), default_backend()
+    )
+
+    with open(filename, "wb") as certfile:
+        certfile.write(public_key.public_bytes(serialization.Encoding.PEM))
+
+    return public_key
